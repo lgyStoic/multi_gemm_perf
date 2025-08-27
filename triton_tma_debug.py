4690f68a4d3b533dc9236902b1fd0afcf9822bb6
garryling <gaoyuanling@meshy.ai>
2025-08-27 09:37:21 +0800

    fix: using d = c + a * b to walkaround

--- triton_tma_debug.py
+++ triton_tma_debug.py
@@ -1,3 +1,4 @@
+from torch.nn.parallel.data_parallel import data_parallel
 import triton
 import triton.language as tl
 import torch
@@ -5,16 +6,16 @@ from typing import Optional
 
 def get_autotune_config():
     return [
-        # triton.Config(
-        #     {
-        #         "BLOCK_SIZE_M": 128,
-        #         "BLOCK_SIZE_N": 256,
-        #         "BLOCK_SIZE_K": 64,
-        #         "GROUP_SIZE_M": 8,
-        #     },
-        #     num_stages=3,
-        #     num_warps=8,
-        # ),
+        triton.Config(
+            {
+                "BLOCK_SIZE_M": 128,
+                "BLOCK_SIZE_N": 256,
+                "BLOCK_SIZE_K": 64,
+                "GROUP_SIZE_M": 8,
+            },
+            num_stages=3,
+            num_warps=8,
+        ),
         triton.Config(
             {
                 "BLOCK_SIZE_M": 256,
@@ -45,6 +46,7 @@ def _matmul_with_base_kernel(
     a_ptr,
     b_ptr,
     c_ptr,
+    d_ptr,
     M, N, K,
     NUM_SMS: tl.constexpr,
     BLOCK_SIZE_M: tl.constexpr,
@@ -74,6 +76,18 @@ def _matmul_with_base_kernel(
         strides=[N, 1],
         block_shape=[BLOCK_SIZE_K, BLOCK_SIZE_N],
     )
+    c_desc = tl.make_tensor_descriptor(
+        c_ptr,
+        shape=[M, N],
+        strides=[N, 1],
+        block_shape=[BLOCK_SIZE_M, BLOCK_SIZE_N],
+    )
+    d_desc = tl.make_tensor_descriptor(
+        d_ptr,
+        shape=[M, N],
+        strides=[N, 1],
+        block_shape=[BLOCK_SIZE_M, BLOCK_SIZE_N],
+    )
 
     # tile_id_c = start_pid - NUM_SMS
     num_pid_in_group = GROUP_SIZE_M * num_pid_n
@@ -83,13 +97,8 @@ def _matmul_with_base_kernel(
         offs_am = pid_m * BLOCK_SIZE_M
         offs_bn = pid_n * BLOCK_SIZE_N
 
-        offs_cm = pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
-        offs_cn = pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
-        c_base_ptr = c_ptr + offs_cm[:, None] * N + offs_cn[None, :]
-        c_mask = (offs_cm[:, None] < M) & (offs_cn[None, :] < N)
-        # c_base = tl.load(c_base_ptr, mask=c_mask, other=0.0)
-        # c_base = c_desc.load([offs_am, offs_bn])
-        c_base = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float16) + 1.0
+        c = c_desc.load([offs_am, offs_bn])
+        d = d_desc.load([offs_am, offs_bn])
 
         accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
         for ki in range(k_tiles):
@@ -97,9 +106,8 @@ def _matmul_with_base_kernel(
             a = a_desc.load([offs_am, offs_k])
             b = b_desc.load([offs_k, offs_bn])
             accumulator = tl.dot(a, b, accumulator)
-
-        c = accumulator.to(tl.float16) * c_base
-        tl.store(c_base_ptr, c, mask=c_mask)
+        d = c * accumulator
+        d_desc.store([offs_am, offs_bn], d.to(tl.float16))
 
 def matmul(a, b, c):
     M, K = a.shape
@@ -114,19 +122,20 @@ def matmul(a, b, c):
 
     triton.set_allocator(alloc_fn)
     NUM_SMS = torch.cuda.get_device_properties("cuda").multi_processor_count
+    d = torch.zeros((M, N), device=a.device, dtype=torch.float16)
 
-    _matmul_with_base_kernel[grid](a, b, c, M, N, K, NUM_SMS)
-    return c
+    _matmul_with_base_kernel[grid](a, b, c, d, M, N, K, NUM_SMS)
+    return d
 
 if __name__ == "__main__":
     torch.manual_seed(11)
-    a = torch.randn(4096, 4096, dtype=torch.float16, device="cuda")
-    b = torch.randn(4096, 4096, dtype=torch.float16, device="cuda")
-    c_base = torch.ones(4096, 4096, dtype=torch.float16, device="cuda") * 1
+    a = torch.randn(1024, 1024, dtype=torch.float16, device="cuda")
+    b = torch.randn(1024, 1024, dtype=torch.float16, device="cuda")
+    c_base = torch.ones(1024, 1024, dtype=torch.float16, device="cuda") * 1
     c_base1 = c_base.clone()
     c = matmul(a, b, c_base)
     c_ref = torch.matmul(a, b) * c_base1
     print(c)
     print(c_ref)
-    assert torch.allclose(c, c_ref)
+    assert torch.allclose(c, c_ref, atol=1e-1)
     print("passed")
